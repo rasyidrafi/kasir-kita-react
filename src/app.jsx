4637c8458c5df2ecb964a6e699fb026569b02a7e
Glitch (glitch-hello-react) <none>
2022-03-21 10:01:21 +0000

    🍽🐪 Checkpoint ./package.json:27065786/2009 ./src/app.jsx:27065786/559 ./src/components/router.jsx:27065786/239

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Router, Link } from "wouter";
 
-/**
-* This code defines the react app
-*
-* Imports the router functionality to provide page navigation
-* Defines the Home function outlining the content on each page
-* Content specific to each page (Home and About) is defined in their components in /pages
-* Each page content is presented inside the overall structure defined here
-* The router attaches the page components to their paths
-*/
-
 // Import and apply CSS stylesheet
 import "./styles/styles.css";
 
@@ -28,11 +18,9 @@ export default function Home() {
       <Seo />
       <main role="main" className="wrapper">
         <div className="content">
-          {/* Router specifies which component to insert here as the main content */}
           <PageRouter />
         </div>
       </main>
-      {/* Footer links to Home and About, Link elements matched in router.jsx */}
       <footer className="footer">
         <div className="links">
           <Link href="/">Home</Link>
