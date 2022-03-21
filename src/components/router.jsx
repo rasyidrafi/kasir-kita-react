70e293df5799f2dd9204a645d5c482c2bcdb1523
Glitch (glitch-hello-react) <none>
2022-03-21 10:13:27 +0000

    🌵👡 Checkpoint ./package.json:27065786/7754 ./index.html:27065786/2619 ./src/components/BasicTextFields.jsx:27065786/480 ./src/pages/login.jsx:27065786/2972 ./src/components/router.jsx:27065786/124

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -2,10 +2,12 @@ import * as React from "react";
 import { Switch, Route, Router } from "wouter";
 import Home from "../pages/home";
 import About from "../pages/about";
+import Login from "../pages/login";
 
 export default () => (
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/login" component={Login} />
     </Switch>
 );
